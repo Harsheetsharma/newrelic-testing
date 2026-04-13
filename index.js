e0ab21f736375c6880d303eaeab43237d48036a9
Harsheetsharma <harshitsharma8012@gmail.com>
2026-04-13 18:02:06 +0530

    Added prometheus and grafana for monitoring

--- index.js
+++ index.js
@@ -17,14 +17,41 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require("express");
+const client = require("prom-client");
+
 const app = express();
 
+const requestCounter = new client.Counter({
+    name: "http_requests_total",
+    help: "Total number of requests",
+});
+
+const requestDuration = new client.Histogram({
+    name: "http_request_duration_seconds",
+    help: "Request duration in seconds",
+    buckets: [0.1, 0.5, 1, 2, 5],
+});
+// middleware to measure time!
+app.use((req, res, next) => {
+    const end = requestDuration.startTimer();
+    res.on("finish", () => {
+        end();
+        requestCounter.inc();
+    });
+    next();
+});
 app.get("/", (req, res) => {
     logger.info("route hit");
     if (Math.random() < 0.5) {
         logger.error("there was an err");
     }
     res.json({ message: "hi there" })
+});
+
+// this is what prometheus will read
+app.get("/metrics", async (req, res) => {
+    res.set("Content-Type", client.register.contentType);
+    res.end(await client.register.metrics());
 });
 
 app.listen(3000, () => {
